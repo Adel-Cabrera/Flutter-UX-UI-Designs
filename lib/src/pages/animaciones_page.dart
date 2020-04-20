a52d95fd594a6dbb280767e674cf63839f7dd196
Adel Cabrera <adel.cabrera.mesina@gmail.com>
2020-04-20 15:57:47 -0400

    added opacity, scaling and translate to widget

--- lib/src/pages/animaciones_page.dart
+++ lib/src/pages/animaciones_page.dart
@@ -25,6 +25,8 @@ class _CuadradoAnimadoState extends State<CuadradoAnimado>
   AnimationController controller;
   Animation<double> rotacion;
   Animation<double> opacidad;
+  Animation<double> moverDerecha;
+  Animation<double> agrandar;
 
   @override
   void initState() {
@@ -36,16 +38,29 @@ class _CuadradoAnimadoState extends State<CuadradoAnimado>
     //rotacion = Tween(begin: 0.0, end: 2.0 * math.pi).animate(controller);
 
     rotacion = Tween(begin: 0.0, end: 2.0 * math.pi).animate(
-      CurvedAnimation(parent: controller, curve: Curves.easeInOutExpo),
+      CurvedAnimation(parent: controller, curve: Curves.easeOut),
     );
 
-    opacidad = Tween(begin: 0.1, end: 1.0).animate(controller);
+    opacidad = Tween(begin: 0.1, end: 1.0).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: Interval(0.0, 0.25, curve: Curves.easeOut),
+      ),
+    );
+
+    moverDerecha = Tween(begin: 0.0, end: 200.0).animate(
+      CurvedAnimation(parent: controller, curve: Curves.easeOut),
+    );
+
+    agrandar = Tween(begin: 0.0, end: 2.0).animate(
+      CurvedAnimation(parent: controller, curve: Curves.easeOut),
+    );
 
     controller.addListener(() {
-      print('Status ${controller.status}');
+      // print('Status ${controller.status}');
       if (controller.status == AnimationStatus.completed) {
-        // controller.reverse();
-        controller.reset();
+        controller.reverse();
+        //controller.reset();
       } else if (controller.status == AnimationStatus.dismissed) {
         controller.forward();
       }
@@ -73,11 +88,17 @@ class _CuadradoAnimadoState extends State<CuadradoAnimado>
       child: _Rectangulo(),
       builder: (context, child) {
         // print(rotacion.value);
-        return Transform.rotate(
-          angle: rotacion.value,
-          child: Opacity(
-            opacity: opacidad.value,
-            child: child,
+        return Transform.translate(
+          offset: Offset(moverDerecha.value, 0.0),
+          child: Transform.rotate(
+            angle: rotacion.value,
+            child: Opacity(
+              opacity: opacidad.value,
+              child: Transform.scale(
+                scale: agrandar.value,
+                child: child,
+              ),
+            ),
           ),
         );
       },
