c572a5b476993429bac627c1761caee6b319414f
Adel Cabrera <adel.cabrera.mesina@gmail.com>
2020-04-22 22:31:36 -0400

    adding opacity to icons in stack

--- lib/src/widgets/emergency_header.dart
+++ lib/src/widgets/emergency_header.dart
@@ -1,6 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class IconHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final Color colorBlanco = Colors.white.withOpacity(0.7);
+
+    return Stack(
+      children: <Widget>[
+        _IconHeaderBackground(),
+        Positioned(
+          top: -50.0,
+          left: -70.0,
+          child: FaIcon(
+            FontAwesomeIcons.plus,
+            size: 250.0,
+            color: colorBlanco,
+          ),
+        ),
+        Column(
+          children: <Widget>[
+            SizedBox(
+              height: 80,
+              width: double.infinity,
+            ),
+            Text(
+              'Haz solicitado',
+              style: TextStyle(fontSize: 20.0, color: colorBlanco),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Asistencia Médica',
+              style: TextStyle(
+                fontSize: 25.0,
+                color: colorBlanco,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            FaIcon(
+              FontAwesomeIcons.plus,
+              size: 80.0,
+              color: Colors.white,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class _IconHeaderBackground extends StatelessWidget {
+  const _IconHeaderBackground({
+    Key key,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Container(
