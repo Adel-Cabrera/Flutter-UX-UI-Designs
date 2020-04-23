aa87204bdfa287adb060c5c9fa7d3603adc86468
Adel Cabrera <adel.cabrera.mesina@gmail.com>
2020-04-23 14:18:33 -0400

    created title

--- lib/src/pages/sliver_page.dart
+++ lib/src/pages/sliver_page.dart
@@ -4,8 +4,98 @@ class SliverListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hello World'),
+      body: _ListaTareas(),
+    );
+  }
+}
+
+class _ListaTareas extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      physics: BouncingScrollPhysics(),
+      itemCount: 20,
+      itemBuilder: (context, index) => _ListItem(),
+    );
+  }
+}
+
+class _Titulo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        SizedBox(
+          height: 30.0,
+        ),
+        Container(
+          margin: EdgeInsets.symmetric(
+            horizontal: 30.0,
+            vertical: 10.0,
+          ),
+          child: Text(
+            'Hello World',
+            style: TextStyle(
+              color: Color(0xFF532128),
+              fontSize: 50.0,
+            ),
+          ),
+        ),
+        Stack(
+          children: <Widget>[
+            SizedBox(
+              width: 100,
+            ),
+            Positioned(
+              bottom: 8,
+              child: Container(
+                width: 150.0,
+                height: 8.0,
+                color: Color(0xFFF7CDD5),
+              ),
+            ),
+            Container(
+              child: Text(
+                'List',
+                style: TextStyle(
+                  color: Color(0xFFD93A30),
+                  fontSize: 50.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class _ListItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      padding: EdgeInsets.all(
+        30.0,
+      ),
+      height: 130.0,
+      margin: EdgeInsets.all(
+        10.0,
+      ),
+      decoration: BoxDecoration(
+        color: Colors.orange,
+        borderRadius: BorderRadius.circular(
+          30.0,
+        ),
+      ),
+      child: Text(
+        'HW',
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 20.0,
+        ),
       ),
     );
   }
