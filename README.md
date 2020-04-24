191dbc891d3f7483f3bf7efe964f371a6508f5f3
Adel Cabrera <adel.cabrera.mesina@gmail.com>
2020-04-24 00:40:18 -0400

    finished app

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ![scApp](ios/Runner/Assets.xcassets/AppIcon.appiconset/scApp.png)
+
+![sc2](ios/Runner/Assets.xcassets/AppIcon.appiconset/sc2.png)
--- lib/src/theme/themechanger.dart
+++ lib/src/theme/themechanger.dart
@@ -6,65 +6,65 @@ class ThemeChanger with ChangeNotifier {
 
   ThemeData _currentTheme;
 
-  ThemeData get currentTheme => _currentTheme;
+  bool get darkTheme => this._darkTheme;
+  bool get customTheme => this._customTheme;
+  ThemeData get currentTheme => this._currentTheme;
 
   ThemeChanger(int theme) {
     switch (theme) {
-      case 1:
+      case 1: // light
         _darkTheme = false;
         _customTheme = false;
-        _currentTheme = ThemeData.light();
+        _currentTheme = ThemeData.light().copyWith(accentColor: Colors.pink);
         break;
-      case 2:
+
+      case 2: // Dark
         _darkTheme = true;
         _customTheme = false;
-        _currentTheme = ThemeData.dark().copyWith(
-          accentColor: Colors.pink,
-        );
+        _currentTheme = ThemeData.dark().copyWith(accentColor: Colors.pink);
         break;
-      case 3:
+
+      case 3: // Dark
         _darkTheme = false;
         _customTheme = true;
         break;
+
       default:
         _darkTheme = false;
-        _customTheme = false;
+        _darkTheme = false;
         _currentTheme = ThemeData.light();
     }
   }
 
-//  set currentTheme(ThemeData value) {
-//    _currentTheme = value;
-//    notifyListeners();
-//  }
-
-  bool get darkTheme => _darkTheme;
-
   set darkTheme(bool value) {
+    _customTheme = false;
     _darkTheme = value;
 
     if (value) {
-      _currentTheme = ThemeData.dark().copyWith(
-        accentColor: Color(0xFF16202B),
-        primaryColorLight: Colors.white,
-        scaffoldBackgroundColor: Color(0xFF16202B),
-        textTheme: TextTheme(
-          body1: TextStyle(
-            color: Colors.white,
-          ),
-        ),
-      );
+      _currentTheme = ThemeData.dark().copyWith(accentColor: Colors.pink);
     } else {
-      _currentTheme = ThemeData.light();
+      _currentTheme = ThemeData.light().copyWith(accentColor: Colors.pink);
     }
 
     notifyListeners();
   }
 
-  bool get customTheme => _customTheme;
-
   set customTheme(bool value) {
     _customTheme = value;
+    _darkTheme = false;
+
+    if (value) {
+      _currentTheme = ThemeData.dark().copyWith(
+        accentColor: Color(0xff48A0EB),
+        primaryColorLight: Colors.white,
+        scaffoldBackgroundColor: Color(0xff16202B),
+        textTheme: TextTheme(body1: TextStyle(color: Colors.white)),
+        // textTheme.body1.color
+      );
+    } else {
+      _currentTheme = ThemeData.light();
+    }
+
     notifyListeners();
   }
 }
